ab3155a778e47a7f3c6f5ae9c350255ea71f55ed
Drew Minns <drew.minns@me.com>
2013-05-23 22:19:29 -0400

    Initial Upload

---
+++ controller.rb
@@ -0,0 +1,25 @@
+layout 'layouts/application.html.haml'
+
+before 'index.html.haml' do
+  @page_index = 0
+  @page_name = "home"
+  layout 'layouts/application.html.haml'
+end
+
+require 'i18n'
+
+I18n.load_path += Dir[File.expand_path('../locales/*.yml', __FILE__)]
+
+helpers do
+  def t(*args)
+    I18n.translate(*args)
+  end
+end
+
+ignore %r(assets/css/_brand_defaults.css.scss)
+ignore %r(assets/css/_mixins.css.scss)
+ignore %r(assets/css/_typography.css.scss)
+ignore %r(assets/css/_forms.css.scss)
+ignore %r(assets/css/_grid.scss)
+ignore %r(partials/*)
+ignore %r(locales)
